2df9bbab7fc525fa70efa0f4cb9860e71917ccb7
Rakshitha05-hub <rakshu3975@gmail.com>
2025-02-18 10:43:03 +0530

    day1_ass3.c

---
+++ day1/day1_ass3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAX_VEHICLES 100
+
+int main() 
+{
+    int n;
+    char car = 'C';  
+    char bike = 'B';  
+    char truck = 'T'; 
+    char type[MAX_VEHICLES];
+    float total_price = 0.0;
+
+    printf("Enter number of vehicles (max %d): ", MAX_VEHICLES);
+    scanf("%d", &n);
+
+    printf("Enter vehicle types (C for car, B for bike, T for truck):\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf(" %c", &type[i]); 
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(type[i] == car)
+        {
+            total_price += 50;
+        }
+        else if(type[i] == bike)
+        {
+            total_price += 20;
+        }
+        else if(type[i] == truck)
+        {
+            total_price += 100;
+        }
+        else
+        {
+            printf("Invalid vehicle type: %c\n", type[i]);
+            return 1;
+        }
+    }
+
+    printf("Total toll collection: %.2f\n", total_price);
+
+    return 0;
+}
